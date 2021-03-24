f97a9065d92686bf86647da05c40fe2983461da5
leandro vallejos <leandro.izqvallejos@gmail.com>
2021-03-23 22:55:50 -0700

    Added Layout and Status Bar Relative to Platform

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View, Platform} from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <View style={{padding:16, backgroundColor:'green'}}>
+          <Text>Search</Text>
+        </View>
+        <View style={{flex:1, padding:16, backgroundColor:'blue'}}>
+          <Text>List</Text>
+        </View>
+      </View>
+      <ExpoStatusBar style="auto"/>
+    </>
   );
 }
 
@@ -15,7 +24,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: isAndroid ? StatusBar.currentHeight: 0,
   },
 });
